bf0587010ca60644295c18fe806d35c02d5ddc6a
abel-baker <seth.keating@gmail.com>
2022-08-11 14:47:29 -0400

    very beginning initial from template

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+
+  return (
+    <header id="header" class="sticky-top mb-2">
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container-sm">
+          <span class="navbar-brand mb-0 h1 text-nowrap">Abel Baker</span>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

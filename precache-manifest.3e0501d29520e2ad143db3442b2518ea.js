19b5554d1a3affa7ad6bde294d5209bf5214b9fd
abel-baker <seth.keating@gmail.com>
2022-08-14 23:55:10 -0400

    Updates

---
+++ precache-manifest.3e0501d29520e2ad143db3442b2518ea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a99222eaf5cd7e08ee65",
+    "url": "/react-portfolio/static/css/main.36a02148.chunk.css"
+  },
+  {
+    "revision": "a99222eaf5cd7e08ee65",
+    "url": "/react-portfolio/static/js/main.a99222ea.chunk.js"
+  },
+  {
+    "revision": "b16611b3ab38568fd369",
+    "url": "/react-portfolio/static/js/1.b16611b3.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "bb2ef2204966e709575eb166ca7338a4",
+    "url": "/react-portfolio/static/media/abel-baker.bb2ef220.jpg"
+  },
+  {
+    "revision": "7c046ebea3b29385ad3fb4943d913164",
+    "url": "/react-portfolio/static/media/js.7c046ebe.webp"
+  },
+  {
+    "revision": "a3d82cd411f1f6b4d3b1f865b5328533",
+    "url": "/react-portfolio/static/media/weatherbyday.a3d82cd4.PNG"
+  },
+  {
+    "revision": "1a710eb6bff1bbae4e6dc821e3869a5d",
+    "url": "/react-portfolio/static/media/gathr.1a710eb6.png"
+  },
+  {
+    "revision": "37d7caa72e42535a37360c378f0fd003",
+    "url": "/react-portfolio/static/media/crochet.37d7caa7.webp"
+  },
+  {
+    "revision": "af3ded709dce73df5420aefc17167366",
+    "url": "/react-portfolio/static/media/uplift.af3ded70.png"
+  },
+  {
+    "revision": "ecc5eb62b393654681bcd376b08a526f",
+    "url": "/react-portfolio/index.html"
+  }
+];

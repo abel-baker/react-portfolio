7b23ac53a8583cf7c162b393177bac211fff4f6f
abel-baker <seth.keating@gmail.com>
2022-08-15 18:38:59 -0400

    minor improvements to individual project rendering

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
-function Project({ id, colSpan = 4, title, subtitle, link, github, titleColor = "#fff", articleClass, divClass, img, filter = "blur(3px)", transform = "", icons }) {
+function Project({ id, colSpan, title, subtitle, link, github, titleColor = "#fff", articleClass, divClass, img, filter = "blur(3px)", transform = "scale(1.1)", icons }) {
 
   return (
-    <div className={`col-lg-${colSpan}`} id={id} key={id}>
+    <div className={colSpan && `col-lg-${colSpan}`} id={id} key={id}>
       <article id={`${id}-card`} className={`card project-card card-cover h-100 overflow-hidden rounded-4 shadow-lg ${articleClass}`}>
         <div className={`d-flex flex-column h-100 p-5 pb-3 ${divClass} text-shadow-2`}>
 
-          <h2 className="pt-5 mt-5 mb-1 display-6 lh-1 fw-bold"><a href={link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: titleColor }}>{title}</a></h2>
+          <h2 className="pt-5 mt-5 mb-1 display-6 lh-1 fw-bold">
+            
+            {link ? 
+            <a href={link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: titleColor }}>
+              <span>{title}</span>
+            </a>
+            : 
+            <span style={{ textDecoration: "none", color: titleColor }}>{title}</span>
+            }
+          </h2>
           <p className="mb-4"><small>{subtitle}</small></p>
           
           <div className="d-flex list-unstyled mt-auto">
             {github &&
               <li className='me-auto' key='github-icon'>
-                <a href={github}><i className='bi-github'></i></a>
+                <a href={github} className={divClass}><i className='bi-github'></i></a>
               </li>
             }
             
